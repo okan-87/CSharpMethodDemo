583d72583eedcb17281d3c2fe91eb368968e4fe0
okan-87 <okankaplan1907@gmail.com>
2022-05-13 16:33:18 +0200

    Add project files.

---
+++ MethodDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Added : " + customer.IdNumber);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted : " + customer.IdNumber);
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Customer informations updated : " + customer.IdNumber);
+        }
+    }
+}
---
+++ MethodDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.IdNumber = "11111";
+            customer.FirstName = "Okan";
+            customer.LastName = "Kaplan";
+            customer.city = "Haarlem";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.IdNumber = "11112";
+            customer2.FirstName = "Yigit";
+            customer2.LastName = "Kaplan";
+            customer2.city = "Ankara";
+
+            CustomerManager manager = new CustomerManager();
+            manager.Add(customer);
+            manager.Add(customer2);
+
+            Customer[] customers = new Customer[] { customer, customer2 };
+            foreach (var item in customers)
+            {
+                Console.WriteLine("You are valuable to us : " + item.FirstName);
+            }
+        }
+    }
+}
